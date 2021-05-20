4c5838943df078cfd7445c8a98adbacfa7f58d91
SimenHus <84506051+SimenHus@users.noreply.github.com>
2021-05-20 12:41:14 +0200

    Create personalization.py

---
+++ personalization.py
@@ -0,0 +1,15 @@
+#Color definition
+grey = (128, 128, 128)
+black = (0, 0, 0)
+white = (255, 255, 255)
+background = white #Variable to be passed to Image class
+textColor = black #Variable to be passed to Image class
+
+#Define image
+fileName = "SFC" #Picture filename
+fileFormat = "png" #Picture file format. PNG recommended
+
+#Standard values
+rect = (200, 200, 0, 0) #(width, height, top left x, top left y)
+frame = (600, 300) #(width, height)
+font = "arial"
